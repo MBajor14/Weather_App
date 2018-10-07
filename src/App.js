fde91a2d2381095cd12e6dd0680e02aa2fc5a394
Mike Bajor <michael.bajor@gmail.com>
2018-10-07 01:58:07 -0400

    Add convert Kelvin to Fahrenheit functionp

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ class App extends Component{
     }
   }
 
+  convertToFahrenheit = (temp) =>{
+    //(temp − 273.15) × 9/5 + 32
+    return (
+      Number((temp - 273.15) * (9/5) + 32).toFixed(2)
+    );
+  }
+
   getWeather = async (event) => {
     event.preventDefault();
     const city = event.target.elements.city.value;
@@ -25,8 +32,10 @@ class App extends Component{
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     if(city && country){
+
+
       this.setState({
-        temperature: data.main.temp,
+        temperature: this.convertToFahrenheit(data.main.temp),
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
@@ -49,16 +58,28 @@ class App extends Component{
   render(){
     return(
       <div>
-        <Title />
-        <Form getWeather={this.getWeather}/>
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-sm-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Title = () => (
     <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
+        <h1 className="title-container_title">Weather Finder</h1>
+        <p className="title-container_subtitle">Find out temperature, conditions and more...</p>
     </div>
 );
 
